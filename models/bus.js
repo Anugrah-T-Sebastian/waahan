5221b691fe522cff8afbf95445cdc4ffce88f9d4
Anugrah Sebastian <87081811+Anugrah-T-Sebastian@users.noreply.github.com>
2024-06-20 13:23:53 +0100

    Bus model created

--- models/bus.js
+++ models/bus.js
@@ -1,3 +1,21 @@
 const mongoose = require("mongoose");
 
-const BusSchema = new mongoose.Schema({});
+const BusSchema = new mongoose.Schema({
+  busId: { type: String, required: true, unique: true },
+  busType: { type: String, required: true },
+  fuelCost: { type: Number, required: true },
+  equipmentCost: { type: Number, required: true },
+  createdOn: { type: Date, default: Date.now },
+  busImageURL: { type: String },
+});
+
+// Middleware to set busId before saving the document
+BusSchema.pre("save", (next) => {
+  if (!this.busId) {
+    this.busId = `bus${this._id}`;
+  }
+  next();
+});
+
+const Bus = mongoose.model("Bus", BusSchema);
+module.exports = Bus;
